74ad0edd8f2af1b26e749ef62e935bd300c37325
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-04-14 14:56:23 -0300

    feat: created fruit class

---
+++ src/model/products/Fruit.java
@@ -0,0 +1,59 @@
+package model.products;
+
+import java.time.LocalDate;
+
+public class Fruit extends FoodProduct {
+    private String variety;
+    private String origin;
+    private boolean seasonal;
+    private String packagingType;
+
+    public Fruit(String cod, String name, double price, int amount,
+                 LocalDate expirationDate, double weight, boolean refrigerated, String nutritionalInfo,
+                 String variety, String origin, boolean seasonal, String packagingType) {
+        super(cod, name, price, amount, expirationDate, weight, refrigerated, nutritionalInfo);
+        this.variety = variety;
+        this.origin = origin;
+        this.seasonal = seasonal;
+        this.packagingType = packagingType;
+    }
+
+    public String getVariety() {
+        return variety;
+    }
+
+    public void setVariety(String variety) {
+        this.variety = variety;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public boolean isSeasonal() {
+        return seasonal;
+    }
+
+    public void setSeasonal(boolean seasonal) {
+        this.seasonal = seasonal;
+    }
+
+    public String getPackagingType() {
+        return packagingType;
+    }
+
+    public void setPackagingType(String packagingType) {
+        this.packagingType = packagingType;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                String.format(" | Variety: %s | Origin: %s | Seasonal: %s | Packaging: %s",
+                        variety, origin, seasonal ? "Yes" : "No", packagingType);
+    }
+}
