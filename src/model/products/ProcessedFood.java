5a70b093d7513b83248a3993678eacca415a1635
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-04-14 15:03:04 -0300

    feat: created processed food class

---
+++ src/model/products/ProcessedFood.java
@@ -0,0 +1,59 @@
+package model.products;
+
+import java.time.LocalDate;
+
+public class ProcessedFood extends FoodProduct {
+    private String category;
+    private String brand;
+    private boolean containsPreservatives;
+    private String cookingInstructions;
+
+    public ProcessedFood(String cod, String name, double price, int amount,
+                         LocalDate expirationDate, double weight, boolean refrigerated, String nutritionalInfo,
+                         String category, String brand, boolean containsPreservatives, String cookingInstructions) {
+        super(cod, name, price, amount, expirationDate, weight, refrigerated, nutritionalInfo);
+        this.category = category;
+        this.brand = brand;
+        this.containsPreservatives = containsPreservatives;
+        this.cookingInstructions = cookingInstructions;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public boolean isContainsPreservatives() {
+        return containsPreservatives;
+    }
+
+    public void setContainsPreservatives(boolean containsPreservatives) {
+        this.containsPreservatives = containsPreservatives;
+    }
+
+    public String getCookingInstructions() {
+        return cookingInstructions;
+    }
+
+    public void setCookingInstructions(String cookingInstructions) {
+        this.cookingInstructions = cookingInstructions;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                String.format(" | Category: %s | Brand: %s | Preservatives: %s | Instructions: %s",
+                        category, brand, containsPreservatives ? "Yes" : "No", cookingInstructions);
+    }
+}
