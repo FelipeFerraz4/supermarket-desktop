b41ec3edf566fca9d1ac3c8b969b2e7397b3e9ea
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2025-04-26 12:20:43 -0300

    feat: created Meat dtos

---
+++ src/dtos/MeatDTO.java
@@ -0,0 +1,22 @@
+package dtos;
+
+import java.time.LocalDate;
+
+public class MeatDTO {
+    private Long id;
+    private String cod;
+    private String name;
+    private Double price;
+    private Integer amount;
+    private LocalDate expirationDate;
+    private Double weight;
+    private Boolean refrigerated;
+    private String nutritionalInfo;
+    private String cutType;
+    private String origin;
+    private Boolean isOrganic;
+    private String animalType;
+    private String storageInstructions;
+
+    // Getters e Setters
+}
