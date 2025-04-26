e1e3d1a514cdb12fea2603dc59ed3a7d9bc21df7
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2025-04-26 12:22:09 -0300

    feat: created hygiene product dtos

---
+++ src/dtos/HygieneProductDTO.java
@@ -0,0 +1,18 @@
+package dtos;
+
+public class HygieneProductDTO {
+    private Long id;
+    private String cod;
+    private String name;
+    private Double price;
+    private Integer amount;
+    private String type;
+    private String brand;
+    private Boolean forSensitiveSkin;
+    private String usageInstructions;
+    private Boolean toxic;
+    private String scent;
+    private Double volume;
+
+    // Getters e Setters
+}
