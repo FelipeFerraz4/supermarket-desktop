5b7821dfc0749b20b94bb6e0155912008f1100f9
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2025-04-26 12:22:36 -0300

    feat: created utensil dtos

---
+++ src/dtos/UtensilDTO.java
@@ -0,0 +1,15 @@
+package dtos;
+
+public class UtensilDTO {
+    private Long id;
+    private String cod;
+    private String name;
+    private Double price;
+    private Integer amount;
+    private String material;
+    private String category;
+    private Boolean reusable;
+    private String size;
+
+    // Getters e Setters
+}
