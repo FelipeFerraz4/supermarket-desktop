187b82218b6ceb913e625dafa4932bf1ffab9a7f
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2025-04-26 12:21:04 -0300

    feat: created process food dtos

---
+++ src/dtos/ProcessedFoodDTO.java
@@ -0,0 +1,21 @@
+package dtos;
+
+import java.time.LocalDate;
+
+public class ProcessedFoodDTO {
+    private Long id;
+    private String cod;
+    private String name;
+    private Double price;
+    private Integer amount;
+    private LocalDate expirationDate;
+    private Double weight;
+    private Boolean refrigerated;
+    private String nutritionalInfo;
+    private String category;
+    private String brand;
+    private Boolean containsPreservatives;
+    private String cookingInstructions;
+
+    // Getters e Setters
+}
