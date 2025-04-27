5b432d353097ac6306acf149c678aa81f2465122
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2025-04-27 14:46:21 -0300

    feat: created call swing

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,41 @@
 import view.console.ConsoleMenu;
+import view.swing.SwingMenu;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ConsoleMenu menu = new ConsoleMenu();
         Scanner scanner = new Scanner(System.in);
-        menu.start(scanner);
+
+        System.out.println("Escolha a interface:");
+        System.out.println("1 - Console");
+        System.out.println("2 - Swing (Interface Gráfica)");
+        System.out.print("Opção: ");
+        int option = Integer.parseInt(scanner.nextLine());
+
+        if (option == 1) {
+            ConsoleMenu menu = new ConsoleMenu();
+            menu.start(scanner);
+        } else if (option == 2) {
+            openSwing();
+        } else {
+            System.out.println("Opção inválida.");
+        }
+
         scanner.close();
+    }
+
+    private static void openSwing() {
+        // Java recomenda que toda criação ou atualização de telas
+        // Swing seja feita dentro da Event Dispatch Thread (EDT) para evitar bugs
+
+        // Cria a janela Swing de forma segura na thread correta EDT
+        SwingUtilities.invokeLater(SwingMenu::display);
+
+        //Mesmo que usar isso
+        //SwingUtilities.invokeLater(() -> {
+        //      SwingMenu.exibir();
+        //});
     }
 }
