2ebae87148f856c4b00f2137549cda61ddeea7c8
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-05-05 00:08:17 -0300

    feat: created update meat

---
+++ src/view/swing/product/update/UpdateMeatPanel.java
@@ -0,0 +1,94 @@
+package view.swing.product.update;
+
+import controllers.PersonController;
+import controllers.ProductController;
+import dtos.MeatDTO;
+import model.people.Person;
+import view.swing.AuxComponents;
+import view.swing.SwingMenu;
+import view.swing.product.SearchProductsPanel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class UpdateMeatPanel extends JPanel {
+    public UpdateMeatPanel(PersonController personController, ProductController productController, Person employee, UUID id) {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        final MeatDTO dto = MeatDTO.toDTO(productController.searchById(id));
+
+        JLabel titleLabel = new JLabel("Atualizar Carne");
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        add(Box.createVerticalStrut(20));
+        add(titleLabel);
+        add(Box.createVerticalStrut(40));
+
+        JTextField codeField = new JTextField(dto.cod());
+        JTextField nameField = new JTextField(dto.name());
+        JTextField priceField = new JTextField(String.valueOf(dto.price()));
+        JTextField amountField = new JTextField(String.valueOf(dto.amount()));
+        JTextField expirationField = new JTextField(dto.expirationDate().toString());
+        JTextField weightField = new JTextField(String.valueOf(dto.weight()));
+        JCheckBox refrigeratedBox = new JCheckBox("Refrigerado", dto.refrigerated());
+        JTextField nutritionalInfoField = new JTextField(dto.nutritionalInfo());
+        JTextField originField = new JTextField(dto.origin());
+        JTextField storageInstructionsField = new JTextField(dto.storageInstructions());
+
+        add(AuxComponents.createHorizontalFields(
+                "Código:", 14, codeField, 200, 25,
+                "Nome:", 14, nameField, 400, 25
+        ));
+        add(AuxComponents.createHorizontalFields(
+                "Preço:", 14, priceField, 200, 25,
+                "Quantidade:", 14, amountField, 200, 25
+        ));
+        add(AuxComponents.createHorizontalFields(
+                "Validade (AAAA-MM-DD):", 14, expirationField, 200, 25,
+                "Peso:", 14, weightField, 200, 25
+        ));
+        add(AuxComponents.createHorizontalFields(
+                "Informações Nutricionais:", 14, nutritionalInfoField, 300, 25,
+                "Origem:", 14, originField, 300, 25
+        ));
+        add(AuxComponents.createLabeledField(
+                "Instruções de Armazenamento:", 14, storageInstructionsField, 300, 25
+        ));
+
+        add(Box.createVerticalStrut(10));
+        add(AuxComponents.createHorizontalCheckBoxes(refrigeratedBox));
+        add(Box.createVerticalStrut(20));
+
+        JButton updateButton = AuxComponents.createStyledButton("Atualizar", 150, 40, () -> {
+            try {
+                MeatDTO updatedDto = dto
+                        .withCode(codeField.getText())
+                        .withName(nameField.getText())
+                        .withPrice(Double.parseDouble(priceField.getText()))
+                        .withAmount(Integer.parseInt(amountField.getText()))
+                        .withExpirationDate(LocalDate.parse(expirationField.getText()))
+                        .withWeight(Double.parseDouble(weightField.getText()))
+                        .withRefrigerated(refrigeratedBox.isSelected())
+                        .withNutritionalInfo(nutritionalInfoField.getText())
+                        .withOrigin(originField.getText())
+                        .withStorageInstructions(storageInstructionsField.getText());
+
+                productController.updateMeat(id, updatedDto);
+                JOptionPane.showMessageDialog(null, "Carne atualizada com sucesso!");
+                SwingMenu.changeScreen(new SearchProductsPanel(personController, productController, employee));
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Erro ao atualizar: " + e.getMessage());
+            }
+        });
+
+        JButton backButton = AuxComponents.createStyledButton("Voltar", 150, 40, () ->
+                SwingMenu.changeScreen(new SearchProductsPanel(personController, productController, employee)));
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(updateButton);
+        buttonPanel.add(backButton);
+        add(buttonPanel);
+    }
+}
