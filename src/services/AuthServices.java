0f9ac9ffa4145b4cd8ed22e7f46e959ff1fc38ef
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-05-07 21:59:33 -0300

    feat: addition exception in auth service

--- src/services/AuthServices.java
+++ src/services/AuthServices.java
@@ -6,12 +6,27 @@ import model.people.Person;
 public class AuthServices {
 
     public static Person login(String email, String password, IRepository<Person> repository) {
+        if (email == null || email.isBlank()) {
+            throw new IllegalArgumentException("Email cannot be null or blank.");
+        }
+
+        if (password == null || password.isBlank()) {
+            throw new IllegalArgumentException("Password cannot be null or blank.");
+        }
+
+        if (repository == null) {
+            throw new IllegalArgumentException("Repository cannot be null.");
+        }
+
         for (Person person : repository.search()) {
-            if (person.getEmail().equalsIgnoreCase(email) &&
+            if (person.getEmail() != null &&
+                    person.getPassword() != null &&
+                    person.getEmail().equalsIgnoreCase(email) &&
                     person.getPassword().equals(password)) {
                 return person;
             }
         }
+
         return null;
     }
 }
