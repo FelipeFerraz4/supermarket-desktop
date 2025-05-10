0ee6724ba6a1d5d49fba4262b73642ed2f2f3573
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-05-10 10:42:50 -0300

    feat: addition exception in employee menu

--- src/view/console/EmployeeView.java
+++ src/view/console/EmployeeView.java
@@ -9,21 +9,26 @@ import java.util.Scanner;
 public class EmployeeView {
 
     public static void menu(Scanner scanner, PersonController personController, ProductController productController) {
-        int option;
+        int option = -1;
         do {
-            System.out.println("\n=== MENU DE PESSOAS ===");
-            System.out.println("1. Pessoas");
-            System.out.println("2. Produtos");
-            System.out.println("0. Voltar");
-            System.out.print("Escolha a opção: ");
-            option = scanner.nextInt();
-            scanner.nextLine();
+            try {
+                System.out.println("\n=== MENU DE PESSOAS ===");
+                System.out.println("1. Pessoas");
+                System.out.println("2. Produtos");
+                System.out.println("0. Voltar");
+                System.out.print("Escolha a opção: ");
+                option = Integer.parseInt(scanner.nextLine());
 
-            switch (option) {
-                case 1 -> PersonView.menu(scanner, personController);
-                case 2 -> ProductView.menu(scanner, productController);
-                case 0 -> System.out.println("Voltando ao menu principal...");
-                default -> System.out.println("Opção inválida.");
+                switch (option) {
+                    case 1 -> PersonView.menu(scanner, personController);
+                    case 2 -> ProductView.menu(scanner, productController);
+                    case 0 -> System.out.println("Voltando ao menu principal...");
+                    default -> System.out.println("Opção inválida.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida! Por favor, insira um número válido.");
+            } catch (Exception e) {
+                System.out.println("Erro inesperado: " + e.getMessage());
             }
         } while (option != 0);
     }
