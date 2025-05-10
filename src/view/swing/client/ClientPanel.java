afc2978880ca6d2608d79c91e06d39f144106248
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-05-10 12:52:45 -0300

    feat: addition exception in client screen

--- src/view/swing/client/ClientPanel.java
+++ src/view/swing/client/ClientPanel.java
@@ -26,50 +26,104 @@ public class ClientPanel extends JPanel {
 
         JButton buttonBeverage = AuxComponents.createStyledButton(
                 "Bebida", 300, 50,
-                () -> SwingMenu.changeScreen(new AddProductToCartPanel(personController, productController, client, cart, ProductType.DRINK))
+                () -> {
+                    try {
+                        SwingMenu.changeScreen(new AddProductToCartPanel(personController, productController, client, cart, ProductType.DRINK));
+                    } catch (Exception e) {
+                        showError("Erro ao abrir categoria Bebida", e);
+                    }
+                }
         );
+
         JButton buttonProcessedFood = AuxComponents.createStyledButton(
                 "Alimento", 300, 50,
-                () -> SwingMenu.changeScreen(new AddProductToCartPanel(personController, productController, client, cart, ProductType.FOOD))
+                () -> {
+                    try {
+                        SwingMenu.changeScreen(new AddProductToCartPanel(personController, productController, client, cart, ProductType.FOOD));
+                    } catch (Exception e) {
+                        showError("Erro ao abrir categoria Alimento", e);
+                    }
+                }
         );
+
         JButton buttonMeat = AuxComponents.createStyledButton(
                 "Carne", 300, 50,
-                () -> SwingMenu.changeScreen(new AddProductToCartPanel(personController, productController, client, cart, ProductType.MEAT))
+                () -> {
+                    try {
+                        SwingMenu.changeScreen(new AddProductToCartPanel(personController, productController, client, cart, ProductType.MEAT));
+                    } catch (Exception e) {
+                        showError("Erro ao abrir categoria Carne", e);
+                    }
+                }
         );
+
         JButton buttonFruit = AuxComponents.createStyledButton(
                 "Fruta", 300, 50,
-                () -> SwingMenu.changeScreen(new AddProductToCartPanel(personController, productController, client, cart, ProductType.FRUIT))
+                () -> {
+                    try {
+                        SwingMenu.changeScreen(new AddProductToCartPanel(personController, productController, client, cart, ProductType.FRUIT));
+                    } catch (Exception e) {
+                        showError("Erro ao abrir categoria Fruta", e);
+                    }
+                }
         );
+
         JButton buttonHygieneProduct = AuxComponents.createStyledButton(
                 "Higiene", 300, 50,
-                () -> SwingMenu.changeScreen(new AddProductToCartPanel(personController, productController, client, cart, ProductType.HYGIENE))
+                () -> {
+                    try {
+                        SwingMenu.changeScreen(new AddProductToCartPanel(personController, productController, client, cart, ProductType.HYGIENE));
+                    } catch (Exception e) {
+                        showError("Erro ao abrir categoria Higiene", e);
+                    }
+                }
         );
+
         JButton buttonUtensil = AuxComponents.createStyledButton(
                 "Utensílio", 300, 50,
-                () -> SwingMenu.changeScreen(new AddProductToCartPanel(personController, productController, client, cart, ProductType.UTENSIL))
+                () -> {
+                    try {
+                        SwingMenu.changeScreen(new AddProductToCartPanel(personController, productController, client, cart, ProductType.UTENSIL));
+                    } catch (Exception e) {
+                        showError("Erro ao abrir categoria Utensílio", e);
+                    }
+                }
         );
 
         add(AuxComponents.createHorizontalButtonPanel(buttonBeverage, buttonProcessedFood));
         add(AuxComponents.createHorizontalButtonPanel(buttonMeat, buttonFruit));
         add(AuxComponents.createHorizontalButtonPanel(buttonHygieneProduct, buttonUtensil));
 
-
         JButton buttonViewCart = AuxComponents.createStyledButton(
                 "Carrinho e Pagamento", 300, 50,
-                () -> SwingMenu.changeScreen(new ViewCartPanel(personController, productController, client, cart))
+                () -> {
+                    try {
+                        SwingMenu.changeScreen(new ViewCartPanel(personController, productController, client, cart));
+                    } catch (Exception e) {
+                        showError("Erro ao abrir o carrinho", e);
+                    }
+                }
         );
 
         add(Box.createVerticalStrut(10));
-        add(AuxComponents.createHorizontalButtonPanel(
-                buttonViewCart
-        ));
-
+        add(AuxComponents.createHorizontalButtonPanel(buttonViewCart));
         add(Box.createVerticalStrut(60));
 
         JButton buttonBack = AuxComponents.createStyledButton(
                 "Voltar", 150, 40,
-                () -> SwingMenu.changeScreen(new MainMenuPanel(personController, productController))
+                () -> {
+                    try {
+                        SwingMenu.changeScreen(new MainMenuPanel(personController, productController));
+                    } catch (Exception e) {
+                        showError("Erro ao voltar para o menu principal", e);
+                    }
+                }
         );
+
         add(buttonBack);
+    }
+
+    private void showError(String message, Exception e) {
+        JOptionPane.showMessageDialog(this, message + ":\n" + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
     }
 }
