8a8923e962f0ef8395df6ef269c924e657490ce3
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-05-10 11:10:17 -0300

    feat: addition exception in swing structure

--- src/view/swing/SwingMenu.java
+++ src/view/swing/SwingMenu.java
@@ -10,21 +10,31 @@ public class SwingMenu {
     private static JFrame frame;
 
     public static void display(PersonController personController, ProductController productController) {
-        frame = new JFrame("Supermercado - Swing");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
-        frame.setLayout(new BorderLayout());
-        frame.setLocationRelativeTo(null);
+        try {
+            frame = new JFrame("Supermercado - Swing");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(800, 600);
+            frame.setLayout(new BorderLayout());
+            frame.setLocationRelativeTo(null);
 
-        changeScreen(new MainMenuPanel(personController, productController));
+            changeScreen(new MainMenuPanel(personController, productController));
 
-        frame.setVisible(true);
+            frame.setVisible(true);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(frame, "Ocorreu um erro ao inicializar a interface: " + e.getMessage(),
+                    "Erro de Inicialização", JOptionPane.ERROR_MESSAGE);
+        }
     }
 
     public static void changeScreen(JPanel novoPainel) {
-        frame.getContentPane().removeAll();
-        frame.getContentPane().add(novoPainel, BorderLayout.CENTER);
-        frame.revalidate();
-        frame.repaint();
+        try {
+            frame.getContentPane().removeAll();
+            frame.getContentPane().add(novoPainel, BorderLayout.CENTER);
+            frame.revalidate();
+            frame.repaint();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(frame, "Ocorreu um erro ao trocar de tela: " + e.getMessage(),
+                    "Erro ao Trocar Tela", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
